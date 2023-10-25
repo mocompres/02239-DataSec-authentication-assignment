77991b5e27ff2c08e5af5767f7e13f8f90b6e1eb
Shania <shaniahau@gmail.com>
2023-10-25 18:56:17 +0200

    changed some classes

--- src/main/java/com/datasec/Client.java
+++ src/main/java/com/datasec/Client.java
@@ -1,10 +1,34 @@
 package com.datasec;
 
 import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.util.Scanner;
+
+import main.java.com.datasec.HelloService;
 
 public class Client {
     public static void main(String[] args) {
         HelloService servce = (HelloService) Naming.lookup("rmi://localhost:5099/hello");
         System.out.println("---"+ servce.echo("hey server"));
     }
+
+	
+	static Scanner input = new Scanner(System.in);
+    
+    public boolean login(PrinterService service) throws RemoteException {
+		String auth = "";
+		
+		while (!auth.equals("Login succesful!")) {	
+		    System.out.println("Enter your username");
+			String userName = input.nextLine();  
+			
+		    System.out.println("Enter your password");
+			String password = input.nextLine();  
+			
+			auth = service.authenticateUser(userName, password);		
+			
+			System.out.println(auth);
+		}
+		return true;
+	}
 }
---
+++ src/main/java/com/datasec/PrintServer.java
@@ -0,0 +1,38 @@
+package main.java.com.datasec;
+
+import java.rmi.RemoteException;
+
+public class PrintServer implements IPrintServer{
+    public String print(String filename, String printer){
+        return null;
+    }
+    public String queue(String printer){
+        return null;
+    }
+    public void topQueue(String printer, int job){
+    }
+
+    public void start(){
+    }
+
+    public void stop(){
+    }
+
+    public void restart(){
+    }
+
+    public String status(String printer){
+        return null;
+    }
+
+    public String readConfig(String parameter){
+        return null;
+    }
+
+    public void setConfig(String parameter, String value){
+    }
+    public String authenticateUser(String username, String password) throws RemoteException{
+        return null;
+    }
+
+}
